52be6c9a8dc39df0da064c970d7238d08c87fcae
ThePibbster <109264606+ThePibbster@users.noreply.github.com>
2023-02-18 18:27:33 -0500

    Bubble Pop A0.2

--- player.gd
+++ player.gd
@@ -1,23 +1,24 @@
 extends KinematicBody2D
 
-export var speed := 10
-export var gravity := 2000
-export var jump := 200
+export var speed := 20
+export var gravity := 500
+export var jump := 100
 var velocity = Vector2()
 
-func get_input():
-	velocity.x = 0
+func _physics_process(delta) -> void:
 	#horizontal movement
+	velocity.x = 0
 	if Input.is_action_pressed("right"):
 		velocity.x = speed
 	if Input.is_action_pressed("left"):
 		velocity.x = -speed
 	
 	#jumping
-	if Input.is_action_just_pressed("up") && is_on_floor(): # and touching the ground
+	if Input.is_action_just_pressed("up") && is_on_floor():
 		velocity.y = -jump
 	
-func _physics_process(delta) -> void:
-	get_input()
 	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("shoot"):
+		print("gay")
